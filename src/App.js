a3e1f468cd6cbc3055984a1ab87a02cd1c6579b2
Fran <fmjerez@icloud.com>
2020-05-13 09:22:00 +0200

    Iteration 3 and 4 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import "bulma/css/bulma.css";
+import Navbar from "../src/navbar/Navbar";
+import FormField from "../src/formfield/Formfield.js";
+import SignUp from "../src/signup/Signup.js";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <>
+      {/* <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        //Estas son las props que le pasamos en FormatField.js
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      /> */}
+      <SignUp />
+    </>
+  );
 };
 
 export default App;
---
+++ src/coolbutton/CoolButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./CoolButton.css";
+
+export const CoolButton = (props) => {
+  return (
+    <>
+      <div className="space">
+        <button className="button is-rounded my-class is-danger is-small ">
+          {props.login}
+        </button>
+      </div>
+      <button className="button is-small is-success">{props.signup}</button>
+    </>
+  );
+};
+export const CoolButtonSubmit = (props) => {
+  return (
+    <input
+      className="button"
+      type="submit"
+      value="Submit input"
+      submit="Enviar Formulario"
+    />
+  );
+};
